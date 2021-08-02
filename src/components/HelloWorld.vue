d0b57a8fe1788dbb7ad616de5dba093f45baa1da
franciscocesar63 <franciscocesar888@gmail.com>
2021-08-02 14:35:28 -0300

    Api CEP implantada e funcionando

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <input
+      type="text"
+      v-bind:id="cep"
+      placeholder="Digite seu Cep"
+      v-on:input="cep = $event.target.value"
+      v-on:keydown="receberCep()"
+    />
+    <span v-show="status">
+      <table style="width: 100%; border: 1px solid black">
+        <tr>
+          <th>CEP</th>
+          <th>Rua</th>
+          <th>Bairro</th>
+          <th>Complemento</th>
+          <th>Cidade</th>
+          <th>UF</th>
+        </tr>
+        <tr>
+          <td>{{ dados.cep }}</td>
+          <td>{{ dados.logradouro }}</td>
+          <td>{{ dados.bairro }}</td>
+          <td>{{ dados.complemento }}</td>
+          <td>{{ dados.localidade }}</td>
+          <td>{{ dados.uf }}</td>
+        </tr>
+      </table>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      cep: "",
+      dados: "",
+      status: false,
+    };
+  },
+
+  methods: {
+    receberCep() {
+      console.log(this.cep);
+      if (this.cep.length > 7) {
+        let url = "https://viacep.com.br/ws/" + this.cep + "/json/";
+        console.log(url);
+        this.$http
+          .get(url)
+          .then((res) => res.json())
+          .then(
+            (dados) => (this.dados = dados),
+            (err) => console.log(err)
+          );
+
+        this.status = true;
+      } else this.status = false;
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
